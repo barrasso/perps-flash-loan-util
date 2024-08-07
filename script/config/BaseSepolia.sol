883d6db050a680ac53128771bb25f129f5d76b21
meb <4982406+barrasso@users.noreply.github.com>
2024-08-06 22:17:43 -0400

    Add deployments and config

---
+++ script/config/BaseSepolia.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.7;
+
+contract BaseSepolia {
+    address constant snxUSD = address(0);
+    address constant USDC = address(0);
+    address constant SYNTHETIX_CORE = address(0);
+    address constant SYNTHETIX_PERPS_MARKET = address(0);
+    address constant SYNTHETIX_SPOT_MARKET = address(0);
+    address constant AAVE_PROVIDER = address(0);
+    address constant UNISWAP_ROUTER = address(0);
+    address constant UNISWAP_QUOTER = address(0);
+}
